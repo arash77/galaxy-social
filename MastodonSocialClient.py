1eb89bc4cfbb23d90c7526ae6cc41ef1b3bcb2df
Arash Kadkhodaei <kad.arash@email.com>
2024-04-04 16:25:36 +0200

    Refactor MastodonSocialClient and add support for mentions, hashtags, images, and alt texts

--- MastodonSocialClient.py
+++ MastodonSocialClient.py
@@ -1,18 +1,27 @@
 from mastodon import Mastodon
 from bs4 import BeautifulSoup
+import textwrap
 
 class mastodon_social_client:
     def __init__(self, access_token=None, api_base_url='https://mstdn.science'):
         self.mastodon_handle = Mastodon(access_token = access_token, api_base_url = api_base_url)
+        self.max_content_length = 500
 
-    def create_post(self, content):
-        if len(content) > 500:
-            content = content[:497] + '...'
-        post = self.mastodon_handle.toot(content)
-        for _ in range(3):
-            try:
-                mastodon_post = self.mastodon_handle.status(post.id)
-                post_content = BeautifulSoup(mastodon_post.content, 'html.parser').get_text(separator=' ')
-                return ''.join(post_content.split()) == ''.join(content.split())
-            except:
-                return False
+    def create_post(self, content, mentions, hashtags, images, alt_texts):
+        content = content + '\n' + mentions + '\n' + hashtags
+        toot_id = None
+        status = []
+        if images:
+            media_ids = []
+            for image in images[:4]:
+                media = self.mastodon_handle.media_post(media_file=image, description=alt_texts[images.index(image)])
+                media_ids.append(media['id'])
+
+        for text in textwrap.wrap(content, self.max_content_length):
+            toot = self.mastodon_handle.status_post(status=text, in_reply_to_id=toot_id, media_ids=media_ids if images and toot_id == None else None)
+            toot_id = toot.id
+            mastodon_post = self.mastodon_handle.status(toot.id)
+            post_content = BeautifulSoup(mastodon_post.content, 'html.parser').get_text(separator=' ')
+            status.append(''.join(post_content.split()) == ''.join(text.split()))
+        
+        return all(status)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from BlueskySocialClient import bluesky_social_client
 from MastodonSocialClient import mastodon_social_client
 from LinkedinSocialClient import linkedin_social_client
+import re
 
 
 bluesky_handle = bluesky_social_client(username=os.environ.get('BLUESKY_USERNAME'), password=os.environ.get('BLUESKY_PASSWORD'))
@@ -14,8 +15,12 @@ linkedin_handle = linkedin_social_client(access_token=os.environ.get('LINKEDIN_A
 
 
 class MetadataHeader:
-    def __init__(self, social_media=[]):
+    def __init__(self, social_media=[], mentions=[], hashtags=[], images=[], alt_texts=[]):
         self.social_media = social_media
+        self.mentions = mentions
+        self.hashtags = hashtags
+        self.images = images
+        self.alt_texts = alt_texts
         
 def parse_markdown_file(file_path):
     with open(file_path, 'r') as file:
@@ -24,11 +29,32 @@ def parse_markdown_file(file_path):
         for line in content.split('\n'):
             if line.startswith('social_media:'):
                 metadata.social_media = line.split(':')[1].strip().replace(' ', '').lower().split(',')
+            if line.startswith('mentions:'):
+                metadata.mentions = line.split(':')[1].strip().replace(' ', '').split(',')
+            if line.startswith('hashtags:'):
+                metadata.hashtags = line.split(':')[1].strip().replace(' ', '').split(',')
+            if line.startswith('images:'):
+                metadata.images = line.split(':')[1].strip().replace(' ', '').split(',')
+            if line.startswith('alt_texts:'):
+                metadata.alt_texts = line.split(':')[1].strip().replace(' ', '').split(',')
+            
         text = content.split('---')[2].lstrip('\n')
         markdown_content = markdown.markdown(text)
         plain_content = BeautifulSoup(markdown_content, 'html.parser').get_text(separator='\n')
         return plain_content, metadata
 
+def fetch_mention_hashtag(metadata, social_media):
+    mentions, hashtags = '', ''
+    for type, text in {'mention': metadata.mentions, 'hashtag': metadata.hashtags}.items():
+        match = re.search(rf'{social_media}\[(.*?)\]', text)
+        if match:
+            values = match.group(1).split(',')
+            if type == 'mention':
+                mentions = ' '.join([f"@{v}" for v in values])
+            elif type == 'hashtag':
+                hashtags = ' '.join([f"#{v}" for v in values])
+    return mentions, hashtags
+    
 
 
 def process_markdown_files():
@@ -44,13 +70,14 @@ def process_markdown_files():
             content, metadata = parse_markdown_file(file_path)
             stats = {}
             for channel in metadata.social_media:
+                mentions, hashtags = fetch_mention_hashtag(metadata, channel)
                 if channel == 'bluesky':
-                    stats[channel] = bluesky_handle.create_post(content)
+                    stats[channel] = bluesky_handle.create_post(content, mentions, hashtags, metadata.images, metadata.alt_texts)
                 elif channel == 'mastodon':
-                    stats[channel] = mastodon_handle.create_post(content)
+                    stats[channel] = mastodon_handle.create_post(content, mentions, hashtags, metadata.images, metadata.alt_texts)
                 elif channel == 'linkedin':
                     stats[channel] = False
-                    # stats[channel] = linkedin_handle.create_post(content)
+                    # stats[channel] = linkedin_handle.create_post(content, mentions, hashtags, metadata.images, metadata.alt_texts)
             processed_files[file_name] = stats
             print(f'Processed {file_name}: {stats}')
     with open('processed_files.json', 'w') as file:

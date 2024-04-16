00ac7d100ea942ff2fe7b47be64c348f4397a810
Arash <arash77.kad@gmail.com>
2024-04-16 12:01:32 +0200

    process all files in posts folder and subfolders

--- lib/main.py
+++ lib/main.py
@@ -2,7 +2,7 @@ import os
 import markdown
 import json
 from bs4 import BeautifulSoup
-import re
+import fnmatch
 import yaml
 import importlib
 
@@ -50,24 +50,28 @@ def process_markdown_files():
     current_folder = os.path.dirname(os.path.abspath(__file__))
     posts_folder = os.path.join(current_folder, "posts")
     processed_files = {}
-    if os.path.exists("processed_files.json"):
-        with open("processed_files.json", "r") as file:
+    if os.path.exists("../processed_files.json"):
+        with open("../processed_files.json", "r") as file:
             processed_files = json.load(file)
-    for file_name in os.listdir(posts_folder):
-        if file_name.endswith(".md") and not processed_files.get(file_name):
-            file_path = os.path.join(posts_folder, file_name)
+
+    for root, _, files in os.walk(posts_folder):
+        for filename in fnmatch.filter(files, "*.md"):
+            file_path = os.path.join(root, filename)
             content, metadata = parse_markdown_file(file_path)
             stats = {}
             for media in metadata["media"]:
+                if file_path in processed_files and media in processed_files[file_path]:
+                    stats[media] = processed_files[file_path][media]
+                    continue
                 mentions = metadata.get("mentions", {}).get(media, [])
                 hashtags = metadata.get("hashtags", {}).get(media, [])
                 images = metadata.get("images", [])
                 stats[media] = plugins[media].create_post(
                     content, mentions, hashtags, images
                 )
-            processed_files[file_name] = stats
-            print(f"Processed {file_name}: {stats}")
-    with open("processed_files.json", "w") as file:
+            processed_files[file_path] = stats
+            print(f"Processed {file_path}: {stats}")
+    with open("../processed_files.json", "w") as file:
         json.dump(processed_files, file)
 
 

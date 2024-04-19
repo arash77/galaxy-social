d768ad5751235d757a6298dec5a9d4bfe06f23f3
Arash <arash77.kad@gmail.com>
2024-04-19 15:38:16 +0200

    debug

--- lib/github_comment.py
+++ lib/github_comment.py
@@ -6,12 +6,14 @@ def comment_to_github(comment_text, error=False):
     github_token = os.getenv("GITHUB_TOKEN")
     repo_owner, repo_name = os.getenv("GITHUB_REPOSITORY").split("/")
     pr_number = os.getenv("GITHUB_REF_NAME").split("/")[0]
+    print(f"Commenting on PR {os.getenv("GITHUB_REF_NAME")}")
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {github_token}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
     url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues/{pr_number}/comments"
+    print(f"Posting comment to {url}")
     data = {"body": comment_text}
     response = requests.post(url, headers=headers, json=data)
     if response.status_code != 201:
